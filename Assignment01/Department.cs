ceb66f309cd02061b258af84a060017e2e7d20a3
DESKTOP-NBJSMBT\User <mahabub@vonome.com>
2024-05-22 16:27:34 +0600

    assignment one

---
+++ Assignment01/Department.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    internal class Department
+    {
+        public Department() {  Students = new List<Student>(); }
+        public string DeptNo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        private List<Student> Students { get; set; }
+        public string AddStudent(Student student)
+        {
+            Student isExist = Students.FirstOrDefault(x => x.RegNo == student.RegNo);
+            if (isExist == null)
+            {
+                Students.Add(student);
+                return "Student Add Successfull";
+            }
+            else
+            {
+                return "Student Add Faild! Because RegNo already exist";
+            }
+        }
+        public string UpdateStudent(Student student)
+        {
+            Student existStudent = Students.FirstOrDefault(x => x.RegNo == student.RegNo);
+            if (existStudent != null)
+            {
+                existStudent.Name=student.Name;
+                return "Student Update Successfull";
+            }
+            else
+            {
+                return "Student Update Faild! Because Sudent do not find";
+            }
+        }
+        public string RemoveStudent(string regNo)
+        {
+            Student student = Students.FirstOrDefault(x => x.RegNo == regNo);
+            if (student != null)
+            {
+                Students.Remove(student);
+                return "Student Remove Successfull";
+            }
+            else
+            {
+                return "Student Remove Faild! Because RegNo donot find";
+            }
+        }
+        public void GetStudentInfo()
+        {
+            List<Student> students = Students.ToList();
+            int sl = 1;
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"Sl No : {sl} Reg No : {student.RegNo} Name : {student.Name}");
+                sl++;
+            }
+
+        }
+    }
+}
---
+++ Assignment01/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Department cseDpt = new Department()
+            {
+                DeptNo = "01",
+                Name = "CSE",
+                Description = "Computer Science & Engineering"
+            };
+            Student mahabub = new Student()
+            {
+                RegNo = "2024s01",
+                Name = "Mahabub Alam"
+            };
+            Student bob = new Student()
+            {
+                RegNo = "2024s02",
+                Name = "Mr Bob"
+            };
+            Student elian = new Student()
+            {
+                RegNo = "2024s03",
+                Name = "Mr Elian"
+            };
+            Student updateSt = new Student()
+            {
+                RegNo = "2024s03",
+                Name = "updateSt"
+            };
+            Console.WriteLine(cseDpt.AddStudent(mahabub));
+            Console.WriteLine(cseDpt.AddStudent(bob));
+            Console.WriteLine(cseDpt.AddStudent(elian));
+            Console.WriteLine(cseDpt.AddStudent(elian));
+            
+            cseDpt.GetStudentInfo();
+            cseDpt.RemoveStudent("2024s02");
+            cseDpt.GetStudentInfo();
+
+
+            Console.WriteLine(cseDpt.UpdateStudent(updateSt));
+            cseDpt.GetStudentInfo();
+
+
+        }
+    }
+}
