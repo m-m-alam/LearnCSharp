3d55b1f17105071c1179d9d98e08ceecb5fd5a34
DESKTOP-NBJSMBT\User <mahabub@vonome.com>
2024-05-24 10:44:59 +0600

    end

---
+++ OOPUniversityManagement/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace OOPUniversityManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            University university = new University()
+            {
+                Name = "DIU",
+                Description = "Daffodill International University",
+            };
+
+            Department cse = new Department()
+            {
+                DeptNo = "CSE01",
+                Name = "Computer Science & Engineerning",
+                Description = "Good Department",
+            };
+
+            Department eee = new Department()
+            {
+                DeptNo = "EEE02",
+                Name = "Electrical & Electronics Engineerning",
+                Description = "Good Department",
+            };
+
+            Console.WriteLine(university.AddDepartment(cse));
+            Console.WriteLine(university.AddDepartment(eee));
+
+            Student mahabub = new Student()
+            {
+                RegNo = "2024cs01",
+                Name = "Mahabub Alam",
+                DeptNo = "CSE01",
+                
+            };
+            Student bob = new Student()
+            {
+                RegNo = "2024cs02",
+                Name = "Mr Bob",
+                DeptNo = "CSE01",
+            };
+            Student elian = new Student()
+            {
+                RegNo = "2024ee01",
+                Name = "Mr Elian",
+                DeptNo = "EEE02",
+            };
+            Student mark = new Student()
+            {
+                RegNo = "2024ee02",
+                Name = "Mark",
+                DeptNo = "EEE02",
+            };
+            Console.WriteLine(cse.AddStudent(mahabub));
+            Console.WriteLine(cse.AddStudent(bob));
+            Console.WriteLine(eee.AddStudent(elian));
+            Console.WriteLine(eee.AddStudent(mark));
+
+            cse.GetStudentInfo();
+            cse.RemoveStudent("2024s02");
+            cse.GetStudentInfo();
+
+            var departments = university.GetDepartmentInfo();
+            int deptSl = 1;
+            int stSl = 1;
+            foreach (Department department in departments)
+            {
+                Console.WriteLine($"Sl No : {deptSl} Reg No : {department.DeptNo} Name : {department.Name}");
+                deptSl++;
+                var students = department.GetStudentByDept(department.DeptNo);
+               
+                foreach (Student student in students)
+                {
+                    Console.WriteLine($"Sl No : {stSl} Reg No : {student.RegNo} Name : {student.Name} Department : {student.DeptNo}");
+                    stSl++;
+                }
+
+            }
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ OOPUniversityManagement/University.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPUniversityManagement
+{
+    internal class University
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        private List<Department> Departments { get; set; }
+
+        public University()
+        {
+            Departments = new List<Department>();
+        }
+
+        public string AddDepartment(Department department)
+        {
+            Department existDepartment = Departments.FirstOrDefault(x => x.DeptNo == department.DeptNo);
+            if (department.DeptNo == null || department.DeptNo =="")
+            {
+                return "Departments Add Faild! Because DeptNo is empty";
+            }
+            if (existDepartment == null)
+            {
+                Departments.Add(department);
+                return "Departments Add Successfull";
+            }
+            else
+            {
+                return "Departments Add Faild! Because RegNo already exist";
+            }
+        }
+        public string UpdateDepartment(Department department)
+        {
+            Department existDepartment = Departments.FirstOrDefault(x => x.DeptNo == department.DeptNo);
+            if (existDepartment != null)
+            {
+                existDepartment.Name = existDepartment.Name;
+                existDepartment.Description = existDepartment.Description;
+                return "Department Update Successfull";
+            }
+            else
+            {
+                return "Department Update Faild! Because Department do not find";
+            }
+        }
+        public string RemoveDepartment(string deptNo)
+        {
+            Department department = Departments.FirstOrDefault(x => x.DeptNo == deptNo);
+            if (department != null)
+            {
+                Departments.Remove(department);
+                return "department Remove Successfull";
+            }
+            else
+            {
+                return "department Remove Faild! Because RegNo donot find";
+            }
+        }
+        public List<Department> GetDepartmentInfo()
+        {
+            List<Department> departments = Departments.ToList();
+            return departments;
+            //int sl = 1;
+            //foreach (Department department in departments)
+            //{
+            //    Console.WriteLine($"Sl No : {sl} Reg No : {department.DeptNo} Name : {department.Name}");
+            //    sl++;
+                
+            //}
+
+        }
+    }
+}
